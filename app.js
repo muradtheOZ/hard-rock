6a1dd6361284580c30fc1f45657f194b986dca41
Murad Ahmed <murad15-9149@diu.edu.bd>
2021-02-11 20:56:10 +0600

    all done with error catch

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
-const searchValue = (name) =>{
+const searchValue = async (name) => {
     const url = (`https://api.lyrics.ovh/suggest/${name}`)
-    fetch (url)
-    .then (res => res.json())
-    .then (data => showMusic(data.data));
+    const res = await fetch(url);
+    const data = await res.json();
+    showMusic(data.data);
+    const inputValue = document.getElementById("searchBox").value = "";
+
 }
-const searchSong = () =>{
+const searchSong = () => {
     const inputValue = document.getElementById("searchBox").value;
     searchValue(inputValue);
-
-    
 }
 
-const showMusic = (data) =>{
+const showMusic = (data) => {
+
     const musicMain = document.getElementById("musicShow");
+    musicMain.innerHTML = "";
     data.forEach(element => {
         console.log(element);
         let musicInfo = document.createElement('div');
         musicInfo.className = "single-result row align-items-center my-3 p-3";
-      let singleMusicInfo = `
+        let singleMusicInfo = `
                     <div class="col-md-9">
-                        <h3 class="lyrics-name">${element.album.title}</h3>
+                        <h3 class="lyrics-name">${element.title}</h3>
                         <p class="author lead">Album by <span>${element.artist.name}</span></p>
+                        <audio controls>
+                        <source src="${element.preview}" type="audio/ogg">
+                        </audio>
                     </div>
+                    
                     <div class="col-md-3 text-md-right text-center">
-                        <button class="btn btn-success">Get Lyrics</button>
+                        <button onclick="getLyrics('${element.artist.name}','${element.title}')" class="btn btn-success">Get Lyrics</button>
                     </div>
        `;
-       musicInfo.innerHTML = singleMusicInfo;
-       console.log(musicInfo);
-       musicMain.appendChild(musicInfo);
+        musicInfo.innerHTML = singleMusicInfo;
+        musicMain.appendChild(musicInfo);
     });
+}
+
+const getLyrics = async (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+    const res = await fetch(url);
+    const data = await res.json();
+
+    let lyrics = document.getElementById("singleLyrics")
+    lyrics.innerText = (data.lyrics);
+
 }
